dca23b231227770247502131c989e9297c423db1
hoangphuclam23102013-stack <hoangphuclam23102013@gmail.com>
2026-03-12 18:56:15 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from pandas.core.interchange.dataframe_protocol import Column
+import streamlit as st
+st.set_page_config(page_title="Trac nghiem tinh cach", page_icon=":question:", layout='wide')
+st.title('Trac nghiem tinh cach')
+st.write('Hay chon 1 con vat, toi se cho ban biet tinh cach cua ban')
+Tinh_cach = {
+    'con meo': 'Ban co ve thich nghi ngoi'
+    'con cho': 'Ban co ve kha chung thanh, nang dong'
+    'con su tu': 'Ban co ve kha manh me, thich dan dau'
+    'con ngua': 'Ban yeu thich su tu do'
+    'thien nga':'Ban co ve ngoai kha la thu hut!'}
+cols = st.column(len(Tinh_cach))
+chon = None
+for i, (con_vat, tinh_cach) in enumerate(Tinh_cach.items()):
+    with cols[i]:
+        if cols[i].button(con_vat):
+            chon = con_vat
+if chon:
+    with st.expander(chon):
+         st.write(Tinh_cach[chon])
+with st.sidebar:
+    st.write('Trac nghiem tinh cach')
+    if chon:
+        st.success(f'Con vat ban chon la: {chon}')
+        st.write(Tinh_cach[chon])
+    else:
+        st.info('Hay chon 1 con vat')
